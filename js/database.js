48a83aa78b482edf728991005adb843b6c2e9c1d
EastsCloud <95631880+EastsCloud@users.noreply.github.com>
2023-01-13 22:32:48 -0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,7 +1,7 @@
 list = [
-    ["0", "Adele", "Smith", "Dormitory1", "easts.cloud.xmzd@outlook.com"],
-    ["1", "Agnes", "Johnson", "Dormitory2", "easts.cloud.xmzd@outlook.com"],
-    ["2", "Billy", "Miller", "Dormitory5", "easts.cloud.xmzd@outlook.com"],
+    ["0", "Adele", "Smith", "Dormitory1", "qingyang.zhu@rectorywolves.org"],
+    ["1", "Agnes", "Johnson", "Dormitory2", "qingyang.zhu@rectorywolves.org"],
+    ["2", "Billy", "Miller", "Dormitory5", "qingyang.zhu@rectorywolves.org"],
     ["3", "Bob", "Kim", "Dormitory3", "qingyang.zhu@rectorywolves.org"],
     ["4", "Calvin", "Davis", "Dormitory1", "qingyang.zhu@rectorywolves.org"],
     ["5", "Christina", "Chen", "Dormitory3", "qingyang.zhu@rectorywolves.org"],
