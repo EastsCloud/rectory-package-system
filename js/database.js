d8b587fb82a199804615d4ad87111feff91cdae2
EastsCloud <95631880+EastsCloud@users.noreply.github.com>
2023-01-13 21:31:12 -0500

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,9 @@
+list = [
+    ["0", "Adele", "Smith", "Dormitory1", "easts.cloud.xmzd@outlook.com"],
+    ["1", "Agnes", "Johnson", "Dormitory2", "easts.cloud.xmzd@outlook.com"],
+    ["2", "Billy", "Miller", "Dormitory5", "easts.cloud.xmzd@outlook.com"],
+    ["3", "Bob", "Kim", "Dormitory3", "qingyang.zhu@rectorywolves.org"],
+    ["4", "Calvin", "Davis", "Dormitory1", "qingyang.zhu@rectorywolves.org"],
+    ["5", "Christina", "Chen", "Dormitory3", "qingyang.zhu@rectorywolves.org"],
+    ["6", "Cindy", "Brown", "Dormitory2", "qingyang.zhu@rectorywolves.org"],
+]
