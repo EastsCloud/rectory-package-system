3ac459231adca4a14913f0c92c3a6ba7127ab88f
EastsCloud <95631880+EastsCloud@users.noreply.github.com>
2023-02-06 14:29:01 -0500

    Delete js directory

--- js/database.js
+++
@@ -1,9 +0,0 @@
-list = [
-    ["0", "Adele", "Smith", "Dormitory1", "qingyang.zhu@rectorywolves.org"],
-    ["1", "Agnes", "Johnson", "Dormitory2", "qingyang.zhu@rectorywolves.org"],
-    ["2", "Billy", "Miller", "Dormitory5", "qingyang.zhu@rectorywolves.org"],
-    ["3", "Bob", "Kim", "Dormitory3", "qingyang.zhu@rectorywolves.org"],
-    ["4", "Calvin", "Davis", "Dormitory1", "qingyang.zhu@rectorywolves.org"],
-    ["5", "Christina", "Chen", "Dormitory3", "qingyang.zhu@rectorywolves.org"],
-    ["6", "Cindy", "Brown", "Dormitory2", "qingyang.zhu@rectorywolves.org"],
-];
